606804b6fbc6d618f904cd3032a737f298877180
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-01-22 02:33:31 +0300

    refactor constructors in HashTable

--- hw01/task01.HashTable/src/main/java/ru/hse/surkov/hw01/HashTable.java
+++ hw01/task01.HashTable/src/main/java/ru/hse/surkov/hw01/HashTable.java
@@ -1,44 +1,41 @@
 package ru.hse.surkov.hw01;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.Arrays;
 
 public class HashTable {
 
     private List[] table;
-    private int tableSize;    // physical size of table
-    private int numberOfKeys; // actual number of keys
+    private int capacity; // physical size of table
+    private int size; // actual number of keys
     private static final int startTableSize = 10;
 
     // fill factor = NUM / DENOM
-    private static final int NUM   = 1; // numerator
+    private static final int NUM = 1; // numerator
     private static final int DENOM = 2; // denominator
-
-    private void clearAndSetTableSize(int tableSize) {
-        this.tableSize = tableSize;
-        numberOfKeys   = 0;
-        table          = new List[tableSize];
-        Arrays.fill(table, new List());
-    }
 
     public HashTable() {
         this(startTableSize);
     }
 
-    private HashTable(int tableSize) {
-        clearAndSetTableSize(tableSize);
+    private HashTable(int capacity) {
+        this.capacity = capacity;
+        size = 0;
+        table = new List[capacity];
+        Arrays.fill(table, new List());
     }
 
     private void copy(HashTable original) {
         table        = original.table;
-        tableSize    = original.tableSize;
-        numberOfKeys = original.numberOfKeys;
+        capacity = original.capacity;
+        size = original.size;
     }
 
     /*
-     * Doubles the tableSize of HashTable
+     * Doubles the capacity of HashTable
      * */
     private void rebuild() {
-        HashTable doubleHashTable = new HashTable(tableSize * 2);
+        HashTable doubleHashTable = new HashTable(capacity * 2);
         for (List chain : table) {
             List.ListVertex[] records = chain.getRecords();
             for (List.ListVertex vertex : records) {
@@ -49,15 +46,15 @@ public class HashTable {
     }
 
     private int getPosition(String key) {
-        int position = key.hashCode() % tableSize;
+        int position = key.hashCode() % capacity;
         if (position < 0) {
-            position += tableSize;
+            position += capacity;
         }
         return position;
     }
 
     public int size() {
-        return numberOfKeys;
+        return size;
     }
 
     public boolean contains(String key) {
@@ -80,9 +77,9 @@ public class HashTable {
         int position = getPosition(key);
         String previousValue = table[position].put(key, value);
         if (previousValue == null) {
-            numberOfKeys++;
+            size++;
         }
-        if (numberOfKeys * (long)DENOM > tableSize * (long)NUM) { // numberOfKeys > tableSize * (NUM / DENOM). Calculations without loss of accuracy
+        if (size * (long)DENOM > capacity * (long)NUM) { // size > capacity * (NUM / DENOM). Calculations without loss of accuracy
             rebuild();
         }
         return previousValue;
@@ -95,12 +92,13 @@ public class HashTable {
         int position = getPosition(key);
         String value = table[position].remove(key);
         if (value != null) {
-            numberOfKeys--;
+            size--;
         }
         return value;
     }
 
     public void clear() {
-        clearAndSetTableSize(1);
+        size = 0;
+        Arrays.fill(table, new List());
     }
 }
