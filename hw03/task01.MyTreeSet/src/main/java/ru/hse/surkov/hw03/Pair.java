45d22f821478143e9f1c3a033f204eafc7d6739d
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-05 01:52:24 +0300

    Add pair

---
+++ hw03/task01.MyTreeSet/src/main/java/ru/hse/surkov/hw03/Pair.java
@@ -0,0 +1,11 @@
+package ru.hse.surkov.hw03;
+
+public final class Pair<F, S> {
+    public F first;
+    public S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+}
