3bbe4de91f348bc5094c693296812d97914eb50b
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-23 22:22:03 +0300

    Add command parsing

--- hw04/task01.DataBase/src/main/java/ru/hse/surkov/hw04/Main.java
+++ hw04/task01.DataBase/src/main/java/ru/hse/surkov/hw04/Main.java
@@ -2,7 +2,7 @@ package ru.hse.surkov.hw04;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Scanner;
 
 public final class Main {
@@ -19,23 +19,30 @@ public final class Main {
             "4 - delete record (name and phone number)\n" +
             "5 - change field 'name' of record\n" +
             "6 - change field 'phone number' of record\n" +
-            "7 - print all records\n";
+            "7 - print all records";
+
+    @NotNull private static String APPLICATION_NAME = "|--------Console phone book--------|";
+    @NotNull private static String DESCIPTION = "Software for storing records consisting of name and phone number";
+    @NotNull private static String ENTER = "enter";
+    @NotNull private static String FIELD_NAME = "name";
+    @NotNull private static String FIELD_PHONE_NUMBER = "phone number";
+    @NotNull private static String ENTER_NAME = ENTER + " " + FIELD_NAME + ": ";
+    @NotNull private static String ENTER_PHONE_NUMBER = ENTER + " " + FIELD_PHONE_NUMBER + ": ";
+    @NotNull private static String DELETING_RECORD = "Deleting record...";
+    @NotNull private static String HAS_BEEN_CHANGED = "has been changed";
 
     private enum CommandInterfaceStatus {FINISHED, IN_PROCESS, READY_TO_WORK};
     @NotNull private static CommandInterfaceStatus commandInterfaceStatus = CommandInterfaceStatus.READY_TO_WORK;
     @NotNull private static Scanner inputScanner = new Scanner(System.in);
+    @NotNull private static PhoneBook phoneBook = new PhoneBook();
 
     public static void main(String[] args) {
-        System.out.println("|--------Console phone book--------|\n" + COMMAND_INTERFACE);
+        System.out.println(APPLICATION_NAME + "\n" + DESCIPTION + "\n" + COMMAND_INTERFACE + "\n");
         commandInterfaceStatus = CommandInterfaceStatus.IN_PROCESS;
+        setPhoneBookName();
         while (commandInterfaceStatus != CommandInterfaceStatus.FINISHED) {
             System.out.print(">");
-            int queryType = -1;
-            try {
-                queryType = inputScanner.nextInt();
-            } catch (Exception ignored) {
-                // Homework statement implies it can not happen
-            }
+            int queryType = inputScanner.nextInt();
             if (queryType == 0) { // exit
                 Main.exit();
             } else if (queryType == 1) { // add record
@@ -58,35 +65,77 @@ public final class Main {
         }
     }
 
-    private static void exit() {
+    public static void setPhoneBookName() {
+        System.out.print("Enter phone book global name: ");
+        String phoneBookName = inputScanner.nextLine();
+        phoneBook.setPhoneBookName(phoneBookName);
+    }
 
+    private static void exit() {
+        commandInterfaceStatus = CommandInterfaceStatus.FINISHED;
     }
 
     private static void addRecord() {
-
+        System.out.print(ENTER_NAME);
+        String name = inputScanner.nextLine();
+        System.out.println(ENTER_PHONE_NUMBER);
+        String phoneNumber = inputScanner.nextLine();
+        phoneBook.addRecord(name, phoneNumber);
     }
 
     private static void findPhoneNumbersByName() {
-
+        System.out.print(ENTER_NAME);
+        String name = inputScanner.nextLine();
+        List<String> phoneNumbers = phoneBook.getPhoneNumbersByName(name);
+        for (int i = 0; i < phoneNumbers.size(); i++) {
+            System.out.println((i + 1) + ". " + phoneNumbers.get(i));
+        }
     }
 
     private static void findNamesByPhoneNumber() {
-
+        System.out.print(ENTER_PHONE_NUMBER);
+        String phoneNumber = inputScanner.nextLine();
+        List<String> names = phoneBook.getNamesByPhoneNumber(phoneNumber);
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println((i + 1) + ". " + names.get(i));
+        }
     }
 
     private static void deleteRecord() {
-
+        System.out.println(ENTER_NAME);
+        String name = inputScanner.nextLine();
+        System.out.println(ENTER_PHONE_NUMBER);
+        String phoneNumber = inputScanner.nextLine();
+        System.out.println(DELETING_RECORD);
+        phoneBook.deleteRecord(name, phoneNumber);
     }
 
     private static void changeFieldNameOfRecord() {
-
+        System.out.println(ENTER_NAME);
+        String name = inputScanner.nextLine();
+        System.out.println(ENTER_PHONE_NUMBER);
+        String phoneNumber = inputScanner.nextLine();
+        System.out.println(ENTER + " new " + FIELD_NAME + ": ");
+        String newName = inputScanner.nextLine();
+        phoneBook.changeName(name, phoneNumber, newName);
+        System.out.println(FIELD_NAME + " " + HAS_BEEN_CHANGED);
     }
 
     private static void changeFieldPhoneNumberOfRecord() {
-
+        System.out.println(ENTER_NAME);
+        String name = inputScanner.nextLine();
+        System.out.println(ENTER_PHONE_NUMBER);
+        String phoneNumber = inputScanner.nextLine();
+        System.out.println(ENTER + " new " + FIELD_PHONE_NUMBER + ": ");
+        String newPhoneNumber = inputScanner.nextLine();
+        phoneBook.changePhoneNumber(name, phoneNumber, newPhoneNumber);
+        System.out.println(FIELD_PHONE_NUMBER + " " + HAS_BEEN_CHANGED);
     }
 
     private static void printAllRecords() {
-
+        System.out.println("Printing...");
+//        for (var record : phoneBook) {
+//            System.out.println(record);
+//        }
     }
 }
