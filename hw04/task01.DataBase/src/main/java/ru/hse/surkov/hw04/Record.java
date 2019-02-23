75664c2b0d72923a2c5a4a6fc66a2f1d3debef87
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-23 23:13:45 +0300

    Add record class

---
+++ hw04/task01.DataBase/src/main/java/ru/hse/surkov/hw04/Record.java
@@ -0,0 +1,29 @@
+package ru.hse.surkov.hw04;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Record {
+    @NotNull private String name;
+    @NotNull private String phoneNumber;
+
+    public Record(@NotNull String name, @NotNull String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @NotNull public String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    @NotNull public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(@NotNull String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
