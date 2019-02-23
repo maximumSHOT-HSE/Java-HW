ca921184802208e01f34b7a33a7b77dd6159e370
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-23 22:22:33 +0300

    Add methods into phonebook class

---
+++ hw04/task01.DataBase/src/main/java/ru/hse/surkov/hw04/PhoneBook.java
@@ -0,0 +1,34 @@
+package ru.hse.surkov.hw04;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PhoneBook {
+
+    public void setPhoneBookName(@NotNull String phoneBookName) {
+
+    }
+
+    public void addRecord(@NotNull String name, @NotNull String phoneNumber) {
+
+    }
+
+    public List<String> getPhoneNumbersByName(String name) {
+        return null;
+    }
+
+    public List<String> getNamesByPhoneNumber(String phoneNumber) {
+        return null;
+    }
+
+    public void deleteRecord(String name, String phoneNumber) {
+    }
+
+    public void changeName(String name, String phoneNumber, String newName) {
+    }
+
+    public void changePhoneNumber(String name, String phoneNumber, String newPhoneNumber) {
+
+    }
+}
