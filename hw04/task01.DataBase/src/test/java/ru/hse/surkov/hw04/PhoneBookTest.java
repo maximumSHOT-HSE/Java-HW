7ea9e39abc17ce3a1e66da9dece9a1063fed81ed
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-23 18:04:40 +0300

    init & configure project for hw04

---
+++ hw04/task01.DataBase/src/test/java/ru/hse/surkov/hw04/PhoneBookTest.java
@@ -0,0 +1,17 @@
+package ru.hse.surkov.hw04;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PhoneBookTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("SetUp");
+    }
+
+    @Test
+    void test() {
+        System.out.println("test");
+    }
+}
