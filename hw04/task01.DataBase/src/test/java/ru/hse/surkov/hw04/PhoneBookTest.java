d2b3846a55cce4e72ef037a10ac311063be07c6e
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-25 00:16:48 +0300

    add two simple tests

--- hw04/task01.DataBase/src/test/java/ru/hse/surkov/hw04/PhoneBookTest.java
+++ hw04/task01.DataBase/src/test/java/ru/hse/surkov/hw04/PhoneBookTest.java
@@ -3,15 +3,34 @@ package ru.hse.surkov.hw04;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.LogManager;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class PhoneBookTest {
+
+    static {
+        LogManager.getLogManager().reset(); // disable all loggers (for testing UI)
+    }
+
+    private PhoneBook phoneBook;
 
     @BeforeEach
     void setUp() {
-        System.out.println("SetUp");
+        phoneBook = new PhoneBook("TEST");
     }
 
     @Test
-    void test() {
-        System.out.println("test");
+    void testContainsNulls() {
+        assertThrows(IllegalArgumentException.class, () -> phoneBook.contains(null, "?"));
+        assertThrows(IllegalArgumentException.class, () -> phoneBook.contains("?", null));
+        assertThrows(IllegalArgumentException.class, () -> phoneBook.contains(null, null));
+    }
+
+    @Test
+    void testAddRecordNulls() {
+        assertThrows(IllegalArgumentException.class, () -> phoneBook.addRecord(null, "?"));
+        assertThrows(IllegalArgumentException.class, () -> phoneBook.addRecord("?", null));
+        assertThrows(IllegalArgumentException.class, () -> phoneBook.addRecord(null, null));
     }
 }
