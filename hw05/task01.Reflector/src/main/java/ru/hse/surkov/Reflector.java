7a6a70e06f4eebc5f4d1d430a0c3431d7a4e5481
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-03-10 20:55:18 +0300

    initial commit (empty project has been created with all configurations including gradle and travis)

---
+++ hw05/task01.Reflector/src/main/java/ru/hse/surkov/Reflector.java
@@ -0,0 +1,29 @@
+package ru.hse.surkov;
+
+/**
+ * Class for creating correct (can be compiled) .java file by Class object.
+ * Also, this class can compare two classes implementations by their class objects
+ * and print differences between them.
+ * */
+public class Reflector {
+
+    /**
+     * Method creates new correct (valid) compilable .java file with
+     * code of someClass.
+     * In the created file all fields, methods, inner and nested classes and interfaces
+     * will be declared with modifiers equivalent to the original class.
+     * Returned types of all methods will be the same as the originals.
+     * Generic methods and inner classes will save their generic entities.
+     * */
+    public void printStructure(Class<?> someClass) {
+
+    }
+
+    /**
+     * Method receives two classes to be compared and prints into standard
+     * output stream (console) all the different fields and methods.
+     * */
+    public void diffClasses(Class<?> leftClass, Class<?> rightClass) {
+
+    }
+}
---
+++ hw05/task01.Reflector/src/test/java/ru/hse/surkov/ReflectorTest.java
@@ -0,0 +1,7 @@
+package ru.hse.surkov;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReflectorTest {
+
+}
